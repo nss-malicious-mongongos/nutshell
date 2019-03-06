a0cdeb1be2e6b67022dbb6e5e8e7a5ca7db4d984
Panya Farnette <naienko@gmail.com>
2019-03-06 12:03:27 -0600

    can edit new tasks

---
+++ src/components/task/TaskEditForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import TaskManager from "../../modules/TaskManager";
+
+export default class TaskForm extends Component {
+    // Set initial state
+    state = {
+        taskName: "",
+        taskDate: ""
+    };
+    
+    // Update state whenever an input field is edited
+    handleFieldChange = event => {
+        const stateToChange = {};
+        stateToChange[event.target.id] = event.target.value;
+        this.setState(stateToChange);
+    };
+    
+    updateExistingTask = event => {
+        event.preventDefault();
+        const editedTask = {
+            id: this.props.match.params.taskId,
+            name: this.state.taskName,
+            completion_date: this.state.taskDate,
+            userId: parseInt(sessionStorage.getItem("credentials")),
+            completed: 0
+        };
+            
+        this.props.updateTask(editedTask)
+            .then(() => this.props.history.push("/"));
+    };
+
+    componentDidMount() {
+        TaskManager.get(this.props.match.params.taskId)
+            .then(task => {
+                this.setState({
+                    taskName: task.name,
+                    taskDate: task.completion_date
+                });
+        });
+    }
+    
+    render() {
+        return (
+            <React.Fragment>
+                <form className="taskForm">
+                    <div className="form-group">
+                        <label htmlFor="taskName">Task name</label>
+                        <input type="text" required className="form-control" onChange={this.handleFieldChange} id="taskName" value={this.state.taskName} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="taskDate">Completion Date</label>
+                        <input type="date" required className="form-control" onChange={this.handleFieldChange} id="taskDate" value={this.state.taskDate} />
+                    </div>
+                    <button type="submit" onClick={this.updateExistingTask} className="btn btn-primary">Submit</button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
