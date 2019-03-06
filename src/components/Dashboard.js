39247965a5db973a6df6d278df33aa6dae1a2d40
Brian Neal <brianbneal@gmail.com>
2019-03-06 11:07:50 -0600

    made Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            //put your components here
+            "Hello world!" //delete this, I just put it here so it wouldn't throw an error
+        )
+    }
+}
