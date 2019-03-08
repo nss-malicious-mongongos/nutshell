c44b437250cb455878bfb5709a7053795f118f02
Panya Farnette <naienko@gmail.com>
2019-03-08 09:07:34 -0600

    experimenting with order of modules on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,15 +12,15 @@ export default class Dashboard extends Component {
         const history = this.props.history;
         return (
             <div className="d-flex flex-wrap h-100" id="dashboard">
-                <ChatList messages={this.props.messages}
+                {/* <ChatList messages={this.props.messages}
                     deleteMessage={this.props.deleteMessage}
                     updateMessage={this.props.updateMessage}
                     {...this.props}
                     history={history}
-                />
-                <MovieList {...this.props} />
+                /> */}
                 <TaskList tasks={this.props.tasks} history={history} updateTask={this.props.updateTask} deleteTask={this.props.deleteTask} />
                 <ArticleList {...this.props} />
+                <MovieList {...this.props} />
                 <FriendList friends={this.props.friends} users={this.props.users} history={history} deleteFriend={this.props.deleteFriend} />
         
             </div>
